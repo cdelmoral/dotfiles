4738db8e688f676fb20d768ed539dc2aceec0670
Carlos del Moral <cdelmoral@mavens.com>
2022-04-07 11:59:34 -0500

    Move salesforce configuration to komodo file

--- zsh/.config/zsh/config.d/komodo.zsh
+++ zsh/.config/zsh/config.d/komodo.zsh
@@ -1,4 +1,41 @@
 ######################################################
+# sfdx
+######################################################
+
+export SFDX_AUTOUPDATE_DISABLE=true
+
+alias dxol="sfdx force:org:list"
+alias dxdu="sfdx force:config:get defaultusername"
+
+# Copies the scratch org login url to the clipboard
+dxopen() {
+    login_command="sfdx force:org:open -r --json"
+
+    if ! [ $# -eq 0 ]
+    then
+        login_command+=" -u $1"
+    fi
+
+    echo "Executing: $login_command"
+
+    login_url=$(eval $login_command | jq -r ".result.url" )
+    echo $login_url | pbcopy
+    echo "Result has been copied to the clipboard."
+}
+
+SFDX_AC_ZSH_SETUP_PATH=/Users/cdelmoral/Library/Caches/sfdx/autocomplete/zsh_setup && test -f $SFDX_AC_ZSH_SETUP_PATH && source $SFDX_AC_ZSH_SETUP_PATH; # sfdx autocomplete setup
+
+######################################################
+# 1Password
+######################################################
+
+# Reads the PLTFM Salesforce Packages Install Key from 1Password
+oppltfm() {
+    eval $(op signin komodohealth)
+    export PLTFM_CORE_INSTALL_KEY=$(op get item 'PLTFM Salesforce Packages Install Key' --fields password)
+}
+
+######################################################
 # Python
 ######################################################
 
@@ -7,8 +44,6 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 if [ -n "$PS1" -a -n "$BASH_VERSION" ]; then source ~/.bashrc; fi
 eval "$(pyenv init -)"
-
-export PATH="/usr/local/opt/awscli@1/bin:$PATH"
 
 ######################################################
 # CumulusCI
