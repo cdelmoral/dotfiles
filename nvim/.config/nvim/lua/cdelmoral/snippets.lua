088fe80c33ac58cf4837dd2f3de7ebaeffd97b83
Carlos del Moral <cdelmoral@mavens.com>
2022-09-02 15:13:21 -0500

    feat: Add snippet for System.assertNotEquals in apex

--- nvim/.config/nvim/lua/cdelmoral/snippets.lua
+++ nvim/.config/nvim/lua/cdelmoral/snippets.lua
@@ -45,5 +45,6 @@ luasnip.add_snippets('apexcode', {
         s('ld', fmt('Logger.debug(\'{} =>\', {});', { i(1, 'variable'), rep(1) })),
         s('sa', fmt('System.assert({});', { i(1, 'isTrue') })),
         s('sae', fmt('System.assertEquals({}, {});', { i(1, 'expected'), i(2, 'actual') })),
+        s('sane', fmt('System.assertNotEquals({}, {});', { i(1, 'notexpected'), i(2, 'actual') })),
 })
 
