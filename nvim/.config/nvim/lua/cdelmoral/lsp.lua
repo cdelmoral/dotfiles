bc93f0794aff95e294832281348bd58923db2383
Carlos del Moral <cdelmoral@mavens.com>
2023-02-23 16:27:49 -0600

    feat: Add rust_analyzer to lsp servers

--- nvim/.config/nvim/lua/cdelmoral/lsp.lua
+++ nvim/.config/nvim/lua/cdelmoral/lsp.lua
@@ -75,7 +75,7 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver', 'eslint', 'jsonls', 'yamlls' }
+local servers = { 'tsserver', 'eslint', 'jsonls', 'yamlls', 'rust_analyzer' }
 
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
