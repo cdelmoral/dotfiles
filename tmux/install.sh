583b960139869b41a1b2252bf5552cd8e5cd58f9
Carlos del Moral <cdelmoral@mavens.com>
2023-03-14 22:51:06 -0500

    feat: Add tmux plugin manager, and dracula plugin to customize status bar

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,2 +1,13 @@
+# Remove and link configuration file
 rm -rf ~/.tmux.conf
 ln -s ~/Projects/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+
+# Initialize and update the tmux plugin manager submodule
+git submodule init
+git submodule update
+
+# Create symbolic link to tpm repo
+ln -s ~/Projects/dotfiles/tmux/plugins/tpm ~/.tmux/plugins
+
+# Install plugins
+~/.tmux/plugins/tpm/scripts/update_plugin.sh
