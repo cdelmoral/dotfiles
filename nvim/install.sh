d9f657c2409fe4e76ceb6202445e529d2c6c94f7
Carlos del Moral <cdelmoral@mavens.com>
2023-03-26 18:32:41 -0500

    feat: Add autoformat configuration on save

--- nvim/install.sh
+++ nvim/install.sh
@@ -3,6 +3,9 @@
 mkdir -p ~/.config/nvim
 cp -a ./kickstart/. ~/.config/nvim
 
+mkdir -p ~/.config/nvim/lua/custom/plugins
+cp ./lua/custom/plugins/*.lua ~/.config/nvim/lua/custom/plugins
+
 mkdir -p ~/.config/nvim/after/plugin
 cp ./after/plugin/*.lua ~/.config/nvim/after/plugin
 
