e9bd0a47b4bd422fefc10faf8f81e3ae65d01a78
Carlos del Moral <cdelmoral@mavens.com>
2023-03-26 20:58:36 -0500

    feat: Add filetype and syntax configuration for apex code and triggers

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -1,16 +1,3 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>x', ':Explore<CR>', { desc = 'E[X]plore local directory' })
-
-local augroup = vim.api.nvim_create_augroup
-
-local apexcode_group = augroup('apexcode', {})
-local autocmd = vim.api.nvim_create_autocmd
-autocmd({ 'BufNewFile', 'BufRead' }, {
-	group = apexcode_group,
-	pattern = { '*.cls', '*.trigger' },
-	callback = function()
-		vim.opt.filetype = 'apexcode'
-		vim.opt.syntax = 'java'
-	end
-})
--- nvim/install.sh
+++ nvim/install.sh
@@ -9,3 +9,8 @@ cp ./lua/custom/plugins/*.lua ~/.config/nvim/lua/custom/plugins
 mkdir -p ~/.config/nvim/after/plugin
 cp ./after/plugin/*.lua ~/.config/nvim/after/plugin
 
+mkdir -p ~/.config/nvim/ftdetect
+cp ./ftdetect/*.vim ~/.config/nvim/ftdetect
+
+mkdir -p ~/.config/nvim/syntax
+cp ./syntax/*.vim ~/.config/nvim/syntax
