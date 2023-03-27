cddcf91a79f1609d85919df3336d753b2de76efd
Carlos del Moral <cdelmoral@mavens.com>
2023-03-26 21:20:37 -0500

    feat: Auto format apex files using vim-prettier

---
+++ nvim/after/plugin/apex.lua
@@ -0,0 +1,9 @@
+local augroup = vim.api.nvim_create_augroup
+
+local apex_group = augroup('apex', {})
+local autocmd = vim.api.nvim_create_autocmd
+autocmd('BufWritePost', {
+	group = apex_group,
+	pattern = { '*.cls', '*.trigger' },
+	command = 'PrettierAsync'
+})
