1c536226f1b987985d3a7c323613fc0bd8c5120c
Carlos del Moral <cdelmoral@mavens.com>
2023-03-26 21:40:40 -0500

    feat: Configure prettier to auto format on save when available

--- nvim/after/plugin/defaults.lua
+++ nvim/after/plugin/defaults.lua
@@ -1,3 +1,6 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set('n', '<leader>x', ':Explore<CR>', { desc = 'E[X]plore local directory' })
+
+vim.g['prettier#autoformat'] = '1'
+vim.g['prettier#autoformat_require_pragma'] = '0'
