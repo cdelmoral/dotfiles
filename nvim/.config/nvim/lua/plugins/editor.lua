044d397f9d2900a00bddeab653d2cdd159720c55
Carlos del Moral <cdelmoral@mavens.com>
2024-02-08 09:26:26 -0600

    feat: Add telescope live grep args dependency and keymap

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -10,11 +10,30 @@ return {
   },
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-live-grep-args.nvim",
+        -- This will not install any breaking changes.
+        -- For major updates, this must be adjusted manually.
+        version = "^1.0.0",
+        config = function()
+          Util.on_load("telescope.nvim", function()
+            require("telescope").load_extension("live_grep_args")
+          end)
+        end,
+      },
+    },
     keys = {
       { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
       { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
       { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
-      { "<leader>sg", Util.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
+      {
+        "<leader>sg",
+        function()
+          require("telescope").extensions.live_grep_args.live_grep_args()
+        end,
+        desc = "Live Grep Args",
+      },
       { "<leader>sG", Util.telescope("live_grep"), desc = "Grep (root dir)" },
     },
   },
