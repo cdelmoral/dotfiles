eae8c0539bf7330db7ebbcfae1e1b020b5bd219f
Carlos del Moral <carderomad@gmail.com>
2025-05-27 17:39:50 -0500

    feat(nvim): Update <leader><space> to search all project

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -20,6 +20,9 @@ return {
   },
   {
     "ibhagwan/fzf-lua",
+    keys = {
+      { "<leader><space>", LazyVim.pick("files", { root = false }), desc = "Find Files (Root Dir)" },
+    },
     opts = {
       winopts = {
         preview = {
